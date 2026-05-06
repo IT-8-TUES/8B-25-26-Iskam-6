cf71df30e6a70b99c2590f22d258f020cad231e9
ivo-shorgov <ivo.shorgov@gmail.com>
2026-05-06 16:36:30 +0300

    Add delivery page and styles

--- shared/loader.js
+++ shared/loader.js
@@ -6,4 +6,16 @@ async function load(id, url) {
 }
 
 // Load header
-load("header", "../shared/header.html");
+load("header", "../shared/header.html").then(() => {
+
+    // delivery
+    document.getElementById('delivery-info').addEventListener('click', function () {
+        window.location.href = '../Ivaylo/delivery.html';
+    });
+    let deliveryLocation = localStorage.getItem('delivery-place');
+    if (!deliveryLocation) {
+        deliveryLocation = "...";
+    }
+    document.getElementById('delivery-info').innerHTML = `Deliver to <b>${deliveryLocation}</b>`;
+
+});
