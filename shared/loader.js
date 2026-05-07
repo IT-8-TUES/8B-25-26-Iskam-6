37b9ff22f2b88f0b133c360b55b10ae9fbe151c8
ivo-shorgov <ivo.shorgov@gmail.com>
2026-05-07 20:21:14 +0300

    Add search function

--- shared/loader.js
+++ shared/loader.js
@@ -28,4 +28,28 @@ load("header", "../shared/header.html").then(() => {
     } else {
         document.getElementById('account').innerHTML = `<b>Sign In ...</b>`;
     }
+
+    //search
+    document.getElementById('search').addEventListener('click', function () {
+        // Get the search input value
+        const searchInput = document.getElementById('search-input');
+        const searchQuery = searchInput.value.trim();
+
+        if (searchQuery) {
+            window.location.href = `../Ivaylo/index.html?q=${encodeURIComponent(searchQuery)}`;
+        } else {
+            window.location.href = '../Ivaylo/index.html';
+        }
+    });
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchQuery = urlParams.get('q');
+
+    if (searchQuery) {
+        const searchInput = document.getElementById('search-input');
+        if (searchInput) {
+            searchInput.value = searchQuery;
+        }
+
+        document.title = `Search: ${searchQuery} - Bamazone`;
+    }
 });
