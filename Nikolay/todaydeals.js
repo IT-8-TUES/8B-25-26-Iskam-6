ca0922f2f0e9422baa3352f2a670514b857bc5b3
Nikolay833 <niki.radni@gmail.com>
2026-05-20 13:22:11 +0300

    Enhance gift cards and deals pages with new styles and features, including badges and original prices

--- Nikolay/todaydeals.js
+++ Nikolay/todaydeals.js
@@ -2,6 +2,8 @@ const products = [
     {
         name: "SOG Voodoo Hawk Mini Axe",
         price: "$35.38",
+        original: "$50.55",
+        badge: "30% OFF",
         img: "../Ivaylo/images/products/axe/axe_main.jpg",
         link: "../Ivaylo/axe.html",
         min: 0, max: 0.2
@@ -9,6 +11,8 @@ const products = [
     {
         name: "Panasonic Inverter Microwave",
         price: "$119.99",
+        original: "$149.99",
+        badge: "20% OFF",
         img: "../Ivaylo/images/products/microwave/microwave_main_view.jpg",
         link: "../Ivaylo/microwave.html",
         min: 0.21, max: 0.4
@@ -16,6 +20,8 @@ const products = [
     {
         name: "Philips Norelco Trimmer",
         price: "$52.49",
+        original: "$69.99",
+        badge: "25% OFF",
         img: "../Ivaylo/images/products/hair_cutter/hair_cutter_main.jpg",
         link: "../Ivaylo/hair_cutter.html",
         min: 0.41, max: 0.7
@@ -23,6 +29,8 @@ const products = [
     {
         name: "EcoClean Premium Sponge Pack",
         price: "$8.49",
+        original: "$9.99",
+        badge: "15% OFF",
         img: "../Ivaylo/images/products/sponges/ChatGPT Image May 13, 2026, 08_18_51 PM.png",
         link: "../Ivaylo/sponge.html",
         min: 0.71, max: 1
@@ -36,10 +44,18 @@ products.forEach(p => {
     if (r >= p.min && r <= p.max) {
         container.innerHTML += `
             <div class="deal-card">
-                <img src="${p.img}" alt="${p.name}">
-                <h3>${p.name}</h3>
-                <p class="sale-price">${p.price}</p>
-                <a href="${p.link}">View Deal</a>
+                <span class="deal-badge">${p.badge}</span>
+                <div class="deal-img-wrap">
+                    <img src="${p.img}" alt="${p.name}">
+                </div>
+                <div class="deal-body">
+                    <h3>${p.name}</h3>
+                    <div class="deal-prices">
+                        <span class="deal-original">${p.original}</span>
+                        <span class="sale-price">${p.price}</span>
+                    </div>
+                    <a href="${p.link}">View Deal</a>
+                </div>
             </div>
         `;
     }
